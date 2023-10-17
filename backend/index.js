e8cf6fc7f434e5e1ae4e35b7b1bd5a85891b313c
Vinit Shukla <vshukla@argusoft.com>
2023-10-17 17:48:48 +0530

    changed port in backend and updated icon of edit and view

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 const Model = require('./model/model');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 
 // Middlewareconst routes = require('./routes/routes');
